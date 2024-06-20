5e2fbfd827d208d03483b9b36dc739610c15d9cb
Rachael Bull <rachaelbull2021@gmail.com>
2024-06-20 08:45:25 +0000

    change debug settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,7 +61,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = ['8000-rachaelbull-drfapi-qh6hlgt76l3.ws-us114.gitpod.io', os.environ.get('ALLOWED_HOST'),]
 
